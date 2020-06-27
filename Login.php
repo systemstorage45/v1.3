f5a4c054b22201dd3f3e01ee0d0565ca40ee0828
systemstorage45 <65388473+systemstorage45@users.noreply.github.com>
2020-06-27 13:31:55 +0700

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,31 @@
+<?php
+function ExitAlert($msg){
+    exit("gg.alert('".$msg."')");
+}
+
+$JDecode = json_decode(file_get_contents('php://input'),true); 
+$FileName = "LoginData.data";
+$ScriptName = "LDOE_1.5.lua";
+$username= $JDecode["Username"];
+$password=  $JDecode["Password"];
+$content =json_decode(file_get_contents($FileName),true);
+if ($content == null){
+$content =[];
+}
+if(isset($username) == false || isset($password)== false ||trim($password) == ""|| trim($username) == ""){
+ExitAlert('█░█░█ █▀█ █▀█ █▄░█ █▀▀   █\n▀▄▀▄▀ █▀▄ █▄█ █░▀█ █▄█   ▄\n\n⚠️ Invalid Username Or Password ⚠️');
+}
+
+
+if($content[$username] <> null){
+	if($content[$username]["password"] == $password){
+exit(file_get_contents($ScriptName));
+}
+else{
+ExitAlert('⚠️ Wrong Password ⚠️');
+}
+	}
+	else{
+		ExitAlert("⚠️ Non Existent User ⚠️");
+		}
+?>
